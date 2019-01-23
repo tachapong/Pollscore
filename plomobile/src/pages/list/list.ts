8f1c89307079c1e6288cd3b975f4c9f685f5da66
powerofwind <powerofwind10@gmail.com>
2019-01-23 15:14:59 +0700

    mobileplo

---
+++ plomobile/src/pages/list/list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+    }
+  }
+
+  
+}
